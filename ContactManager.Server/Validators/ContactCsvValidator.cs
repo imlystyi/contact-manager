ff83577617916ffca909084e7b966f52dd942e21
imlystyi <vladte3c@gmail.com>
2024-04-05 21:51:05 +0300

    fix(server): fix validator problem when every validation session was failed

--- ContactManager.Server/Validators/ContactCsvValidator.cs
+++ ContactManager.Server/Validators/ContactCsvValidator.cs
@@ -1,4 +1,5 @@
-﻿using ContactManager.Server.Models.Dto;
+﻿using System.Text.RegularExpressions;
+using ContactManager.Server.Models.Dto;
 using FluentValidation;
 
 namespace ContactManager.Server.Validators;
@@ -7,11 +8,7 @@ public class ContactCsvValidator : AbstractValidator<ContactCsvInputDto>
 {
     public ContactCsvValidator()
     {
-        this.RuleFor(x => x.Name).NotEmpty().MaximumLength(120);
-        this.RuleFor(x => x.BirthDate).NotEmpty();
-        this.RuleFor(x => x.IsMarried).NotEmpty();
-        this.RuleFor(x => x.Phone).NotEmpty().MaximumLength(20)
-            .Matches(@"((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}");
-        this.RuleFor(x => x.Salary).NotEmpty();
+        this.RuleFor(x => x.Name).MaximumLength(120);
+        this.RuleFor(x => x.Phone).MaximumLength(20);
     }
 }
