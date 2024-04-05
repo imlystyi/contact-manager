c4202e625b7b610341bebd5e1d805003c7a11dcb
imlystyi <vladte3c@gmail.com>
2024-04-05 21:53:13 +0300

    feat(client): provide main UI functionality

--- contactmanager.client/src/App.jsx
+++ contactmanager.client/src/App.jsx
@@ -1,49 +1,148 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-    const [forecasts, setForecasts] = useState();
+  const [contacts, setContacts] = useState([]);
 
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
+  useEffect(() => {
+    getAllContacts();
+  }, []);
 
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tabelLabel">
+  const deleteButtonClicked = async (id) => {
+    await deleteContact(id);
+    await getAllContacts();
+  };
+
+  const fileChange = async (event) => {
+    const file = event.target.files[0];
+    const fileContent = await readFileContent(file);
+    await addContactCsv(fileContent);
+    await getAllContacts();
+  };
+
+  const readFileContent = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const contents = event.target.result;
+        resolve(contents);
+      };
+      reader.onerror = (error) => {
+        reject(error);
+      };
+      reader.readAsText(file);
+    });
+  };
+
+  async function getAllContacts() {
+    const response = await fetch("https://localhost:7071/api/get-all");
+    const data = await response.json();
+    setContacts(data);
+  }
+
+  async function addContactCsv(csv) {
+    const response = await fetch("https://localhost:7071/api/add-csv", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(csv),
+    });
+
+    return response;
+  }
+
+  async function deleteContact(id) {
+    const response = await fetch(`https://localhost:7071/api/delete/${id}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+
+    return response;
+  }
+
+  return (
+    <div className="app-container">
+      <h1>Contact Manager</h1>
+      <input type="file" onChange={fileChange} />
+      <div className="content-container">
+        {contacts.length === 0 ? (
+          <p>
+            <em>Loading...</em>
+          </p>
+        ) : (
+          <table>
             <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Date of Birth</th>
+                <th>Married</th>
+                <th>Phone</th>
+                <th>Salary</th>
+                <th></th>
+              </tr>
             </thead>
             <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
+              {contacts.map((contact) => (
+                <tr key={contact.id}>
+                  <td>{contact.id}</td>
+                  <td>
+                    <input
+                      id="inputName"
+                      className="inputName"
+                      type="text"
+                      placeholder={contact.name}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      className="inputBirthDate"
+                      type="text"
+                      placeholder={contact.birthDate}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      className="inputIsMarried"
+                      type="text"
+                      placeholder={contact.isMarried}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      className="inputPhone"
+                      type="text"
+                      placeholder={contact.phone}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      className="inputSalary"
+                      type="text"
+                      placeholder={contact.salary}
+                    />
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => deleteButtonClicked(contact.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
-        </table>;
-
-    return (
-        <div>
-            <h1 id="tabelLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
-        </div>
-    );
-    
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        setForecasts(data);
-    }
+          </table>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
